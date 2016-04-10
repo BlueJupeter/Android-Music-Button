7fd41f293e938dcad803baa5b2170f37ea12d6fd
BlueJupeter <piotr.bejgrowicz@gmail.com>
2016-04-10 20:22:57 +0200

    Initial commit

---
+++ app/src/main/java/com/jupiter/blue/musicbutton/MainActivity.java
@@ -0,0 +1,33 @@
+package com.jupiter.blue.musicbutton;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final MediaPlayer hivesMP = MediaPlayer.create(this, R.raw.hate_to_say);
+
+        Button playHate = (Button) this.findViewById(R.id.play_hate);
+
+               playHate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(hivesMP.isPlaying()) {
+                   hivesMP.pause();
+                } else {
+                    hivesMP.start();
+                }
+
+            }});
+
+    }
+}
